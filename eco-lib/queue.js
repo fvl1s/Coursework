bb92cb2002420789757d273a84b50a4a66a208d0
Вадим-IM54 <golubkovvaadim@gmail.com>
2026-04-02 16:45:00 +0300

    fix dequeue using splice

--- eco-lib/queue.js
+++ eco-lib/queue.js
@@ -20,9 +20,16 @@ class SensorQueue {
   }
 
   dequeueHighest() {
-    const item = this.peekHighest();
-    this.items.pop();
-    return item;
+    if (this.items.length === 0) return null;
+    let maxIdx = 0;
+    for (let i = 1; i < this.items.length; i++) {
+      if (this.items[i].priority > this.items[maxIdx].priority) {
+        maxIdx = i;
+      }
+    }
+    const result = this.items[maxIdx];
+    this.items.splice(maxIdx, 1);
+    return result;
   }
 
 }
