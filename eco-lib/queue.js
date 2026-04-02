a833355f589cffdba9e52129fd1da945ee4e49f5
Вадим-IM54 <golubkovvaadim@gmail.com>
2026-04-02 13:21:00 +0300

    add priority queue

---
+++ eco-lib/queue.js
@@ -0,0 +1,15 @@
+class SensorQueue {
+  constructor() {
+    this.items = [];
+    this.counter = 0;
+  }
+  enqueue(item, priority) {
+    this.counter++;
+    this.items.push({
+      data: item,
+      priority: priority,
+      id: this.counter
+    });
+  }
+}
+module.exports = { SensorQueue };
