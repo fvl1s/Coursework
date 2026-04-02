f8e62e696c95d52ce21959c231dba3bd927ed371
Вадим-IM54 <golubkovvaadim@gmail.com>
2026-04-02 14:12:00 +0300

    implement priority peek

--- eco-lib/queue.js
+++ eco-lib/queue.js
@@ -5,11 +5,17 @@ class SensorQueue {
   }
   enqueue(item, priority) {
     this.counter++;
-    this.items.push({
-      data: item,
-      priority: priority,
-      id: this.counter
-    });
+    this.items.push({ data: item, priority: priority, id: this.counter });
+  }
+
+  peekHighest() {
+    let max = this.items[0];
+    for (let i = 1; i < this.items.length; i++) {
+      if (this.items[i].priority > max.priority) {
+        max = this.items[i];
+      }
+    }
+    return max;
   }
 }
 module.exports = { SensorQueue };
