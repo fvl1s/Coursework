0d4041cd9857c951e8e4cfbc5f467ec8612e4203
Вадим-IM54 <golubkovvaadim@gmail.com>
2026-04-02 20:15:00 +0300

    forgot export

--- eco-lib/index.js
+++ eco-lib/index.js
@@ -1,9 +1,11 @@
 const { getValues } = require("./generators");
 const { limitTime } = require("./iterators");
 const { memoize } = require("./utils");
+const { SensorQueue } = require("./queue");
 
 module.exports = {
   getValues,
   limitTime,
-  memoize
+  memoize,
+  SensorQueue
 };
