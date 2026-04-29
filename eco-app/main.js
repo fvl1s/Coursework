afec8ee4ec3735a8676a9cdd0bfb0db543bd9822
Вадим-IM54 <golubkovvaadim@gmail.com>
2026-04-29 17:55:10 +0300

    add stream test to console app

--- eco-app/main.js
+++ eco-app/main.js
@@ -1,20 +1,44 @@
-const { getValues, limitTime, memoize } = require("eco-lib");
+const { 
+    getValues, 
+    limitTime, 
+    memoize, 
+    createSensorStream, 
+    processStream 
+} = require("eco-lib");
 
 const getStatus = memoize((val) => {
-  if (val > 80) return "DANGER";
-  if (val > 50) return "WARNING";
-  return "NORMAL";
+    if (val > 80) return "DANGER";
+    if (val > 50) return "WARNING";
+    return "NORMAL";
 }, { limit: 5, ttl: 4000 });
 
 async function start() {
-  console.log("MyEco: Запуск моніторингу...");
-  
-  for await (const val of limitTime(15)) {
-    const status = getStatus(val);
-    console.log(`Отримано: ${val} ppm | Статус: ${status}`);
-  }
+    console.log("Monitoring started ...");
+    
+    const vals = getValues();
+    await limitTime(vals, 3, (val) => {
+        const status = getStatus(val);
+        console.log(`Received: ${val} ppm | Status: ${status}`);
+    });
 
-  console.log("MyEco: Роботу завершено");
+    console.log("End of data.");
 }
 
-start();
+async function testStream() {
+    console.log("Monitoring started ...");
+    const stream = createSensorStream("CO2", 3);
+    
+    await processStream(stream, (data) => {
+        console.log(`[${data.time}] ${data.name}: ${data.val}`);
+    });
+    
+    console.log("End of data.");
+}
+
+async function init() {
+    await testStream();
+    console.log("-----------------------");
+    await start();
+}
+
+init();
