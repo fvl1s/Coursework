2e139fb64c14101619235a253cdcc875fda72c94
Вадим-IM54 <golubkovvaadim@gmail.com>
2026-05-12 14:24:10 +0300

    add try-catch and error callback in limitTime

--- eco-lib/iterators.js
+++ eco-lib/iterators.js
@@ -2,10 +2,14 @@ async function limitTime(iterator, seconds, callback) {
   const limit = seconds * 1000;
   const start = Date.now();
 
-  for (const val of iterator) {
-    if (Date.now() - start > limit) break;
-    callback(val);
-    await new Promise((res) => setTimeout(res, 500));
+  try {
+    for await (const val of iterator) {
+      if (Date.now() - start > limit) break;
+      callback(null, val);
+      await new Promise((res) => setTimeout(res, 500));
+    }
+  } catch (err) {
+    callback(err, null);
   }
 }
 
